98c35138ab21a2764db97f98a21f623bb64d5b08
quantum-apps <102760289+quantum-apps@users.noreply.github.com>
2022-04-01 09:28:52 -0700

    Update app_map.py

--- app_map.py
+++ app_map.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('ruta al csv ...')
+df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
 
 st.write(df)
